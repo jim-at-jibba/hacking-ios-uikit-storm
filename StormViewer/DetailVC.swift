fc30eb91f66424ed08f0d3ef4f76108c0567fc34
James Best <jim@justjibba.net>
2023-07-27 09:02:34 +0100

    Add image VC and view

---
+++ StormViewer/DetailVC.swift
@@ -0,0 +1,31 @@
+//
+//  DetailVC.swift
+//  StormViewer
+//
+//  Created by James Best on 27/07/2023.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
